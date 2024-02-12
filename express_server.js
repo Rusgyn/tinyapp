598ab331eebff280d0ffa99a7cb56f1dfc1340e0
rusgyn <rusgynmagsanay@gmail.com>
2024-02-12 16:33:58 -0500

    Start new server with express

---
+++ express_server.js
@@ -0,0 +1,16 @@
+const express = require("express");// Import the express library
+const app = express();// Define our app as an instance of express
+const PORT = 8080; //default port 8080
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello");//Respond "Hello" when a GET request is made to the homepage
+});
+
+app.listen(PORT, () => {//Make the server listen to PORT, which is 8080.
+  console.log(`Example app listening on port ${PORT}!`);
+});
