de6f95172a8e28dc1e12d6af88e80f369e1e2e7b
rusgyn <rusgynmagsanay@gmail.com>
2024-02-13 11:26:17 -0500

    Add view engine, EJS

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require("express");// Import the express library
 const app = express();// Define our app as an instance of express
 const PORT = 8080; //default port 8080
 
+app.set("view engine", "ejs");
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
