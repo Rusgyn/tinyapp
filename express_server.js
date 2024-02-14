5c3fce8b390cdf795495bae36cdba0c12fae54d2
rusgyn <rusgynmagsanay@gmail.com>
2024-02-14 15:46:06 -0500

    Add a POST route to receive the form submission

--- express_server.js
+++ express_server.js
@@ -2,14 +2,21 @@ const express = require("express");// Import the express library
 const app = express();// Define our app as an instance of express
 const PORT = 8080; //default port 8080
 
-app.set("view engine", "ejs");//This tells the Express app to use EJS as its templating engine
+app.set("view engine", "ejs");//This tells the Express app to use EJS as its templating engine.
+
+app.use(express.urlencoded({ extended: true }));//Express's built-in middleware function urlencoded will convert the request body from a Buffer into string that we can read
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.use(express.urlencoded({ extended: true }));//Express's built-in middleware function urlencoded will convert the request body from a Buffer into string that we can read
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
+
 
 app.get("/", (req, res) => {
   res.send("Hello");//Respond "Hello" when a GET request is made to the homepage
