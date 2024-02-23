d6bcdf9ac06ede6c38c8618be4b38f87a8893554
rusgyn <rusgynmagsanay@gmail.com>
2024-02-23 15:06:44 -0500

    code that test helper functions

---
+++ test/helpersTest.js
@@ -0,0 +1,40 @@
+const { assert } = require('chai');
+
+const { getUserByEmail, urlsForUser  } = require('../helper_functions/helpers');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
+describe('getUserByEmail', function() {
+  it('should return a user with valid email', function() {
+    const user = getUserByEmail("user@example.com", testUsers)
+    const expectedUserID = "userRandomID";
+    // Write your assert statement here
+    assert.equal(user.id, expectedUserID);
+  });
+
+  it('should return a user with valid email', function() {
+    const user = getUserByEmail("user2@example.com", testUsers)
+    const expectedUserID = "user2RandomID";
+    // Write your assert statement here
+    assert.equal(user.id, expectedUserID);
+  });
+
+  it('should return undefined when provided with non-existent email', function() {
+    const user = getUserByEmail("unregistered@email.com", testUsers)
+    const expectedUserID = undefined;
+    // Write your assert statement here
+    assert.equal(user, expectedUserID);
+  });
+
+});
