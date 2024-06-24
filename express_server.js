17801fb2953ab1cfaffe24819376320de08ce9cf
rusgyn <rusgynmagsanay@gmail.com>
2024-06-24 16:54:19 -0400

    Implement tinyapp web server with express

---
+++ express_server.js
@@ -0,0 +1,16 @@
+const express = require("express"); // use the express module
+const app = express(); // Define app as instance of the express module.
+const PORT = 8080; //Default port 80880
+
+const urlDatabase = {
+  b2xVn2: "http://www.lighthouselabs.ca",
+  "9sm5xk": "http://www.google.com",
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello! Welcome to TinyApp");
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on pport ${PORT}!`);
+});
