ed4431ab2b48587ddc5dd262aa3f0b493c194ea0
rusgyn <rusgynmagsanay@gmail.com>
2024-06-24 17:02:18 -0400

    Add new route representing the entire urlDatabse object in JSON string

--- express_server.js
+++ express_server.js
@@ -11,6 +11,10 @@ app.get("/", (req, res) => {
   res.send("Hello! Welcome to TinyApp");
 });
 
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
 app.listen(PORT, () => {
   console.log(`App listening on pport ${PORT}!`);
 });
