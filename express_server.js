8a3d8a9eb29d9abeb1180509fb88f5bce1f3b884
rusgyn <rusgynmagsanay@gmail.com>
2024-06-25 16:10:36 -0400

    New route handler that use id to lookup its associated longURL from database

--- express_server.js
+++ express_server.js
@@ -24,12 +24,24 @@ app.get("/hello", (req, res) => {
   res.send("<html> <body> Hello <b>World!</b> </body> </html>")
 });
 
+//Route handler for "/urls"
 app.get("/urls", (req, res) => {
   const templateVars = {
     urls: urlDatabase
   };
+
   res.render("urls_index", templateVars);
 });
+
+//Route handler, use id from route parameter to lookup it's associated longURL from the urlDatabase
+app.get("/urls/:id", (req, res) => {
+    const templateVars = {
+    id: req.params.id, 
+    longURL: urlDatabase[req.params.id]
+  };
+  res.render("urls_show", templateVars);
+});
+
 
 //Make the server listen on our define port, 8080
 app.listen(PORT, () => {
