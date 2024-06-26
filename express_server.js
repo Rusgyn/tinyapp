783129443919d97d301b5dbedf77ce54d5797ab4
rusgyn <rusgynmagsanay@gmail.com>
2024-06-26 12:42:20 -0400

    Implement .get login to render /urls

--- express_server.js
+++ express_server.js
@@ -97,6 +97,10 @@ app.post("/urls/:id/delete", (req, res) => {
 
   res.redirect("/urls"); //After the resource has been deleted, redirect the client back to the urls_index page
 })
+//READ - Route that shows the index page where user can login.
+app.get("/login", (req, res) => {
+  res.render("/urls");
+})
 
 //CREATE - POST route that handles login.
 app.post("/login", (req, res) => {
