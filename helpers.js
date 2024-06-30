c23bfe8f912873dce297d342242c041c33878de5
rusgyn <rusgynmagsanay@gmail.com>
2024-06-30 16:32:14 -0400

    Move all helper functions for TinyApp server for cleaner code.

--- helpers.js
+++ helpers.js
@@ -1,4 +1,12 @@
-// Helper Function
+// This file contains all the helper functions for TinyApp server.
+
+//Imported the database
+const { users, urlDatabase } = require('./database/dBaseHelpers');
+
+//Function that generates random alphanumeric characters.
+const generateRandomString = (length) => {
+  return Math.random().toString(36).substring(2, length);
+};
 
 //To get the user object by email
 const getUserByEmail = (email, database) => {
@@ -11,6 +19,30 @@ const getUserByEmail = (email, database) => {
       return database[key]; //return the user's object
     }
   }
+  return null;
 };
 
-module.exports = { getUserByEmail };
+//To get the user by id
+const getUser = (userId) => {
+  return users[userId];
+};
+
+// To check if user is logged in, return Boolean.
+const isUserLoggedIn = (reqSession) => {
+  return (reqSession.user_id);
+  // return (reqSession.user_id ? true : false);
+};
+
+// To get the urls associated with the user
+const urlsForUser = (userId) => {
+  let urls = {};
+  for (const [urlId, url] of Object.entries(urlDatabase)) {
+    //urlId = is the key; url = is the value {longURL:.., userID:..}
+    if (userId === url.userID) {
+      urls[urlId] = url;
+    }
+  }
+  return urls;
+};
+
+module.exports = { generateRandomString, getUserByEmail, getUser, isUserLoggedIn, urlsForUser };
